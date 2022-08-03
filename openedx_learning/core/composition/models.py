97c1d6c8e34e78944e6f04763dd2605ef15e4b54
Feanil Patel <feanil@tcril.org>
2022-08-03 14:36:14 -0400

    feat: Add a Segment model that builds on top of the Item models.
    Co-authored-by: David Ormsbee <dave@tcril.org>

--- openedx_learning/core/composition/models.py
+++ openedx_learning/core/composition/models.py
@@ -9,85 +9,100 @@ There are broadly three types of UserPartitions:
 """
 from django.db import models
 
-# from ..publish.models import LearningObjectVersion
-
 from openedx_learning.lib.fields import hash_field, identifier_field, immutable_uuid_field
 
-"""
-class LearningContextTypePartitionGroup(models.Model):
-    learning_context_type = models.ForeignKey(LearningContextType, on_delete=models.CASCADE)
-    partition_id = models.BigIntegerField(null=False)
-    group_id = models.BigIntegerField(null=False)
+from ..itemstore.models import ItemVersion
+from ..publishing.models import LearningContext, LearningContextVersion
 
 
-class LearningContextVersionPartitionGroup(models.Model):
-    learning_context_version = models.ForeignKey(LearningContextVersion, on_delete=models.CASCADE)
-    partition_id = models.BigIntegerField(null=False)
-    group_id = models.BigIntegerField(null=False)
-"""
+class Segment(models.Model):
+    """
+    Segments are the lowest renderable component in a learning context.
 
+    Most of the layout of a Segment follows closely to that of an item. The biggest
+    difference is that a SegmentVersion will "contain" a list of orered ItemVersions.
+    """
 
-#class Block(models.Model):
-#    """
-#    A Block is the simplest possible piece of content. It has no children.#
-#    How do we decompose this? Separate tables for different aspects, like grades?
-#    """
-#
-#    learning_object_version = models.ForeignKey(LearningObjectVersion, on_delete=models.CASCADE)
-
-
-class Unit(models.Model):
-    identifier = identifier_field()
-
-
-class UnitVersion(models.Model):
     uuid = immutable_uuid_field()
+    learning_context = models.ForeignKey(LearningContext, on_delete=models.CASCADE)
+
+    # Mutable app defined identifier.
     identifier = identifier_field()
-    hash = hash_field()
 
-
-class UnitContentBlock(models.Model):
-
-
-    pass
-
-
-
-class Partition(models.Model):
-    """
-    Each row represents a Partition for dividing content into PartitionGroups.
-
-    UserPartitions is a pluggable interface. Some IDs are static (with values
-    less than 100). Others are dynamic, picking a range between 100 and 2^31-1.
-    That means that technically, we could use IntegerField instead of
-    BigIntegerField, but a) that limit isn't actually enforced as far as I can
-    tell; and b) it's not _that_ much extra storage, so I'm using BigInteger
-    instead (2^63-1).
-
-    It's a pluggable interface (entry points: openedx.user_partition_scheme,
-    openedx.dynamic_partition_generator), so there's no "UserPartition" model.
-    We need to actually link this against the values passed back from the
-    partitions service in order to map them to names and descriptions.
-    Any CourseSection or CourseSectionSequence may be associated with any number
-    of UserPartitionGroups. An individual _user_ may only be in one Group for
-    any given User Partition, but a piece of _content_ can be associated with
-    multiple groups. So for instance, for the Enrollment Track user partition,
-    a piece of content may be associated with both "Verified" and "Masters"
-    tracks, while a user may only be in one or the other.
-    """
-    partition_num = models.BigIntegerField(null=False)
-    
-
-class PartitionGroup(models.Model):
-    """
-    """
-    partition = models.ForeignKey(Partition, on_delete=models.CASCADE)
-    group_num = models.BigIntegerField(null=False)
+    created = manual_date_time_field()
+    modified = manual_date_time_field()
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                name='unique_ol_publishing_partition_partition_group_num',
-                fields=['partition_id', 'group_num'],
+                fields=["learning_context", "identifier"],
+                name="segment_uniq_lc_identifier",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.identifier}"
+
+
+class SegmentVersion(models.Model):
+    """
+    A particular version of a Segment.
+
+    A new version should be created when there is a change to the set of items in
+    a segment.
+
+    Question for Dave: Why should this map to item versions instead of item? When would we
+    reference the Item model directly?
+    """
+
+    uuid = immutable_uuid_field()
+
+    segment = models.ForeignKey(Segment, on_delete=models.CASCADE)
+
+    segment_items = models.ManyToManyField(
+        ItemVersion,
+        through="SegmentVersionItemVersion",
+        related_name="segment_versions",
+    )
+
+    learning_context_versions = models.ManyToManyField(
+        LearningContextVersion,
+        through="LearningContextVersionSegmentVersion",
+        related_name="segment_versions",
+    )
+
+
+class LearningContextVersionSegmentVersion(models.Model):
+    learning_context_version = models.ForeignKey(
+        LearningContextVersion, on_delete=models.CASCADE
+    )
+    segment_version = models.ForeignKey(SegmentVersion, on_delete=models.RESTRICT)
+
+    segment = models.ForeignKey(Segment, on_delete=models.RESTRICT)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["learning_context_version", "segment_version"],
+                name="lcvsv_uniq_lcv_segment_version",
             ),
+            models.UniqueConstraint(
+                fields=["learning_context_version", "segment"],
+                name="lcvsv_uniq_lcv_segment",
+            ),
+        ]
+
+
+class SegmentVersionItemVersion(models.Model):
+    segment_version = models.ForeignKey(SegmentVersion, on_delete=models.CASCADE)
+    item_version = models.ForeignKey(ItemVersion, on_delete=models.RESTRICT)
+
+    order_num = models.PositiveIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["segment_version", "order_num"],
+                name="sviv_uniq_segment_item_order",
+            )
         ]
