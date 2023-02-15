e6918480ac647d7744c609c7af5b514128e9e713
David Ormsbee <dave@edx.org>
2023-02-15 10:24:21 -0500

    refactor: admin helper for ReadOnlyModel

--- openedx_learning/core/components/admin.py
+++ openedx_learning/core/components/admin.py
@@ -12,17 +12,7 @@ from .models import (
     Content,
     PublishedComponent,
 )
-
-
-class ReadOnlyModelAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+from openedx_learning.lib.admin import ReadOnlyModelAdmin
 
 
 class ComponentVersionInline(admin.TabularInline):
